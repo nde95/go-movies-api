73018b75b45ebba0bd53394e478dcac615265373
nde95 <ndennison268@gmail.com>
2023-05-19 10:09:50 -0400

    generating jwt auth token/secret on route

---
+++ cmd/api/auth.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type Auth struct {
+	Issuer        string
+	Audience      string
+	Secret        string
+	TokenExpiry   time.Duration
+	RefreshExpiry time.Duration
+	CookieDomain  string
+	CookiePath    string
+	CookieName    string
+}
+
+type jwtUser struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+type TokenPairs struct {
+	Token        string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+type Claims struct {
+	jwt.RegisteredClaims
+}
+
+func (j *Auth) GenerateTokenPair(user *jwtUser) (TokenPairs, error) {
+	// Generate token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = fmt.Sprintf("%s %s", user.FirstName, user.LastName)
+	claims["sub"] = fmt.Sprint(user.ID)
+	claims["aud"] = j.Audience
+	claims["iss"] = j.Issuer
+	claims["iat"] = time.Now().UTC().Unix()
+	claims["typ"] = "JWT"
+	// set expiration
+	claims["exp"] = time.Now().UTC().Add(j.TokenExpiry).Unix()
+
+	// make signed token
+	signedAccessToken, err := token.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	//create refresh token
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	refreshTokenClaims := refreshToken.Claims.(jwt.MapClaims)
+	refreshTokenClaims["sub"] = fmt.Sprint(user.ID)
+	refreshTokenClaims["iat"] = time.Now().UTC().Unix()
+
+	// set expiration
+	refreshTokenClaims["exp"] = time.Now().UTC().Add(j.RefreshExpiry).Unix()
+
+	// create signed refresh token
+	signedRefreshToken, err := refreshToken.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+	// make token pairs
+	var tokenPairs = TokenPairs{
+		Token:        signedAccessToken,
+		RefreshToken: signedRefreshToken,
+	}
+
+	return tokenPairs, nil
+}
