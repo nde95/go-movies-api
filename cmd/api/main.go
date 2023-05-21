89d19c4df6cd9452ce4f031fd119afaef3d299b8
nde95 <ndennison268@gmail.com>
2023-05-21 12:36:15 -0400

    pulling posters from tmdb

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,6 +21,7 @@ type application struct {
 	JWTIssuer    string
 	JWTAudience  string
 	CookieDomain string
+	APIKey       string
 }
 
 func main() {
@@ -34,6 +35,7 @@ func main() {
 	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
 	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
 	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
+	flag.StringVar(&app.APIKey, "api-key", "c53032546e7ade1ee00c63226498965e", "API key")
 	flag.Parse()
 	// connect to postgres
 	conn, err := app.connectToDB()
